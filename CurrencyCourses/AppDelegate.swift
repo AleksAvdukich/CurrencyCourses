40f3fd3180867fc8be12f336e6fc336c5b2bd58b
Aleksandr Avdukich <avdukichsanel@mail.ru>
2019-01-22 02:10:04 +0300

    Course List

--- CurrencyCourses/AppDelegate.swift
+++ CurrencyCourses/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-    Model.shared.parseXML()
+    Model.shared.parseXML() //при запуске приложения загружаем курсы
     
     return true
   }
